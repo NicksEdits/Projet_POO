b127c469f61d9e48f955afe15af0505a906a83d5
NicksEdits <61987116+NicksEdits@users.noreply.github.com>
2022-12-03 13:18:34 +0100

    Update VueControleurGyromite.java

--- src/VueControleur/VueControleurGyromite.java
+++ src/VueControleur/VueControleurGyromite.java
@@ -57,7 +57,7 @@ public class VueControleurGyromite extends JFrame implements Observer {
     private JLabel[][] tabJLabel; // cases graphique (au moment du rafraichissement, chaque case va être associée à une icône, suivant ce qui est présent dans le modèle)
 
     private Timer timer;
-    private int timeSecond = 10;   //Set ici le temps de jeu de base
+    private int timeSecond = 100;   //Set ici le temps de jeu de base
     private JLabel score ;
 
     public VueControleurGyromite(Jeu _jeu) {
